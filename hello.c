90518e89fd5ad7d45ab84ea207488693af9b9060
Swimpus <yejo32602@gmail.com>
2026-05-13 19:22:29 +0000

    Initial Commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h> // Similar to summoning a library in Python
+                   // Ergo import math, import random.
+                   // #include <stdio.h> means summoning standard input ouput.
+
+int main(void) // The "void" is the parameter we are passing. In this case, nothing.
+{
+    printf("Hello,world!\n"); // Most statements in C end with a semicolon.
+}
+
+//  $ gcc -o hello hello.c
+    // Saves the file as a command known as "hello"
+// $ ./hello
+    // Executes the processes done in file.
+
+
